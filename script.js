e316e53e59232638269f89f5c22c42e7ec31023a
RogerMoe <new.rogermoe@gmail.com>
2023-11-29 23:52:44 +0700

    adding portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+
+    var popup = document.getElementById('menu');
+    popup.classList.toggle('active')
+
+}
+
+function toggle1(){
+    var green = document.getElementById('green');
+    green.classList.toggle('active')
+
+    var box = document.getElementById('box');
+    box.classList.toggle('active')
+
+
+}
